1e13139ea08a1ffe6eade5e6bb53220133e79802
Jex <JuanCode24@gmail.com>
2025-10-16 12:46:08 -0700

    Se agrego obtenerCategorias en el handler

--- backend/api/handlers/CategoriaHandler.go
+++ backend/api/handlers/CategoriaHandler.go
@@ -12,6 +12,8 @@ func CategoriaHandler(w http.ResponseWriter, r *http.Request){
 	switch r.Method {
 	case http.MethodPost:
 		crearCategoria(w,r)
+	case http.MethodGet:
+		obtenerCategorias(w,r)
 	default:
 		http.Error(w,"Metodo no permitido",http.StatusMethodNotAllowed)
 	}
@@ -37,4 +39,32 @@ func crearCategoria(w http.ResponseWriter, r *http.Request){
 		"message": "Categoria creada con exito",
 		"data": categoria,
 	})
+}
+
+func obtenerCategorias(w http.ResponseWriter, r *http.Request){
+	var categorias []models.Categoria
+
+
+	result := config.DB.Find(&categorias)
+	if result.Error != nil {
+		http.Error(w, "Error al obtener categorias", http.StatusInternalServerError)
+		return
+	}
+
+	if len(categorias) == 0 {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"message": "No hay categorias registradas",
+			"data":		[]models.Categoria{},
+		})
+		return
+	}
+
+
+	w.Header().Set("Content-type", "application/json")
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"message": "Categorias obtenidas correctamente",
+		"data": categorias, 
+	})
+
 }
